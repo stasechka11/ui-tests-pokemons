08b9877486e99a22e471949c01841016f9c5f180
Anastasia Fokina <45092650+stasechka11@users.noreply.github.com>
2026-01-29 13:10:08 +0400

    LoginPage

--- src/test/java/com/example/pages/LoginPage.java
+++ src/test/java/com/example/pages/LoginPage.java
@@ -2,6 +2,7 @@ package com.example.pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
@@ -13,9 +14,52 @@ public class LoginPage {
     private final By usernameInput = By.id("k_email");
     private final By passwordInput = By.id("k_password");
     private final By submitButton = By.cssSelector("button[type='submit']");
+    private final By errorMessage = By.cssSelector("");
 
     public LoginPage(WebDriver driver) {
         this.driver = driver;
         this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    }
+
+    //Открыть страницу
+    public LoginPage open(String url) {
+        driver.get(url);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(usernameInput));
+        return this;
+    }
+
+    //Ввести логин
+    public LoginPage enterUsername(String username) {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(usernameInput))
+                .clear();
+        driver.findElement(usernameInput).sendKeys(username);
+        return this;
+    }
+
+    //Ввести пароль
+    public LoginPage enterPassword(String password) {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(passwordInput))
+                .clear();
+        driver.findElement(usernameInput).sendKeys(password);
+        return this;
+    }
+
+    //Нажать логин
+    public MainPage clickLogin() {
+        wait.until(ExpectedConditions.elementToBeClickable(submitButton)).click();
+        return new MainPage(driver);
+    }
+
+    //Метод логина
+    public MainPage login(String username, String password) {
+        return this
+                .enterUsername(username)
+                .enterPassword(password)
+                .clickLogin();
+    }
+
+    //Проверка сообщения об ошибке
+    public boolean isErrorVisible() {
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(errorMessage)).isDisplayed();
     }
 }
---
+++ src/test/java/com/example/pages/MainPage.java
@@ -0,0 +1,16 @@
+package com.example.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class MainPage {
+    private final WebDriver driver;
+    private final WebDriverWait wait;
+
+    public MainPage(WebDriver driver) {
+        this.driver = driver;
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    }
+}
